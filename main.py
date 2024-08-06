765dbba6a40d32a5e875e637cb827d4e4db8011d
aracho <byaracho@gmail.com>
2024-08-06 16:27:02 +0900

    add: sqlmodel 적용

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+
+from database.connection import conn
 from routes.users import user_router
 from routes.events import event_router
 
@@ -7,6 +9,12 @@ import uvicorn
 app = FastAPI()
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+@app.on_event("startup")
+def on_startup():
+    conn()
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
